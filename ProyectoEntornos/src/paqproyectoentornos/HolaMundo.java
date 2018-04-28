41424e1623784c34d8bb82b35af4f84b4a8d202d
sandl <sandl@DESKTOP-I35VJA8>
2018-04-28 08:36:58 +0200

    Cambio en la misma línea sin actualizar (mikel)

--- ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
+++ ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo, aquí va el segundo commit");
-		System.out.println("Segundo usuario escribe nueva línea sin tener el proyecto actualizado");
+		System.out.println("Segundo usuario escribe nueva línea sin tener el BIEN proyecto actualizado");
 		System.out.println("Primer usuario hace un commit and push con esta nueva línea");
 	}
 
