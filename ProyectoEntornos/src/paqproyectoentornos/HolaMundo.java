2f8d09a5ea9e6084c3ab4b244d345b644b64a3a7
sandl <sandl@DESKTOP-I35VJA8>
2018-04-28 07:29:22 +0200

    Nueva línea del primer usuario (sandlovi)

--- ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
+++ ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo, aquí va el segundo commit");
+		System.out.println("Primer usuario hace un commit and push con esta nueva línea");
 	}
 
 }
