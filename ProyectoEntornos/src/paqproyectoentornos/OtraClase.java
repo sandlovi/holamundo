91abe031a0521a0b0db4ff549201f16ad89dabc7
sandl <sandl@DESKTOP-I35VJA8>
2018-04-28 11:03:00 +0200

    Correción de error en texto

--- ProyectoEntornos/src/paqproyectoentornos/OtraClase.java
+++ ProyectoEntornos/src/paqproyectoentornos/OtraClase.java
@@ -3,6 +3,6 @@ package paqproyectoentornos;
 public class OtraClase {
 
 	public void metodoDeOtraRama() {
-		System.out.println("Este texto viene de un método en otra rama");
+		System.out.println("Este texto viene de un método en otra clase");
 	}
 }
