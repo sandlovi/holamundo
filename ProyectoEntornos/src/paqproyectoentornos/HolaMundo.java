b6691936afe0b8d15d8f1ac69a242c5e3e959817
sandl <sandl@DESKTOP-I35VJA8>
2018-04-28 07:36:19 +0200

    Nueva línea del segundo usuario (mikel)

--- ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
+++ ProyectoEntornos/src/paqproyectoentornos/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo, aquí va el segundo commit");
+		System.out.println("Segundo usuario escribe nueva línea sin tener el proyecto actualizado");
 	}
 
 }
